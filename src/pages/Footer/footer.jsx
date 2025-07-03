14daab9f6164048fabdf985338f468fc87409412
Mani Bansal <bansalmani013@gmail.com>
2025-07-03 18:55:50 +0530

    changes

--- src/pages/Footer/footer.jsx
+++ src/pages/Footer/footer.jsx
@@ -5,23 +5,21 @@ import NTU from '../../assets/images/NTU.jpg'
 function Footer() {
     return (
         <div>
-        <div className="footer-ntu">
-            <div>
-            <img src={NTU} style={{height: '100px', width: '100px'}}/>
+            <div className="whole-footer">
+                <div className="ntu-logo">
+                    <img src={NTU} style={{ height: '100px', width: 'auto' }} />
+                </div>
+                <div className="footer">
+
+                    <img src={lablogo} style={{ height: '100px', width: 'auto',marginBottom:"10px" }} />
+                    <h5>Das Lab</h5>
+                    <h6>Anuvab Das</h6>
+                    <h6>Assistant Professor</h6>
+                    <h6>School of Chemistry, Chemical Engineering & Biotechnology</h6>
+                    <h6>Nanyang Technological University</h6>
+
+                </div>
             </div>
-        
-        </div>
-        <div className="footer">
-            <div>
-            <img src={lablogo} style={{height: '100px', width: '100px'}}/>
-            <h5>Das Lab</h5>
-            <h6>Anuvab Das</h6>
-            <h6>Assistant Professor</h6>
-            <h6>School of Chemistry, Chemical Engineering & Biotechnology</h6>
-            <h6>Nanyang Technological University</h6>
-            </div>
-        
-        </div>
         </div>
     )
 }
